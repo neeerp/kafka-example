5c4c6c7decc4c2ed9989f9d0fd6c89b1ea155349
David Goldstein <goldstein.g.david@gmail.com>
2023-09-09 11:16:16 -0400

    Implement kafka consumer

--- kafka_consumer/src/main.rs
+++ kafka_consumer/src/main.rs
@@ -1,3 +1,25 @@
+use kafka::consumer::{Consumer, FetchOffset};
+use std::str;
+
+const KAFKA_HOST: &str = "localhost:29092";
+
 fn main() {
-    println!("Hello, world!");
+    let hosts = vec![KAFKA_HOST.to_owned()];
+
+    let mut consumer = Consumer::from_hosts(hosts)
+        .with_topic("topic-name".to_owned())
+        .with_fallback_offset(FetchOffset::Latest)
+        .create()
+        .unwrap();
+
+    loop {
+        for ms in consumer.poll().unwrap().iter() {
+            for m in ms.messages() {
+                println!("{:?}", str::from_utf8(m.value).unwrap());
+            }
+
+            consumer.consume_messageset(ms).unwrap();
+        }
+        consumer.commit_consumed().unwrap();
+    }
 }
