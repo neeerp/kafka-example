909991bd959af6cad92f15f4c6aaf9c7cbf69cc1
David Goldstein <goldstein.g.david@gmail.com>
2023-09-09 11:30:34 -0400

    Implement kafka producer

--- kafka_producer/src/main.rs
+++ kafka_producer/src/main.rs
@@ -1,3 +1,17 @@
+use kafka::producer::{Producer, Record};
+
+const KAFKA_HOST: &str = "localhost:29092";
+
 fn main() {
-    println!("Hello, world!");
+    let hosts = vec![KAFKA_HOST.to_owned()];
+
+    let mut producer = Producer::from_hosts(hosts).create().unwrap();
+
+    for i in 0..10 {
+        let buf = format!("{i}");
+        producer
+            .send(&Record::from_value("topic-name", buf.as_bytes()))
+            .unwrap();
+        println!("Sent: {i}");
+    }
 }
